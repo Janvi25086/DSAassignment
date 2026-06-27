40db7c6a5e2e83b7874decf129d26b133d58dc3c
Janvi25086 <janviii1505@gmail.com>
2026-06-27 20:48:51 +0530

    First commit

---
+++ Day1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+
+        // Read array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int pos = sc.nextInt();
+        int x = sc.nextInt();
+
+        for (int i = n; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[pos - 1] = x;
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}

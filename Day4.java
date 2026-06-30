0f35f76487e09e46d2e89a0d08e8b9ac67e6296d
Janvi25086 <janviii1505@gmail.com>
2026-06-30 21:15:51 +0530

    fourth commit

---
+++ Day4.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Day4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read array size
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Read array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Reverse the array using two pointers
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        // Print the reversed array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}

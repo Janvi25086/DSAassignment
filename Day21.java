c56c36890df5dd278e16a96e032c2bb24afc4cb6
Janvi25086 <janviii1505@gmail.com>
2026-07-17 15:39:28 +0530

    twenty one commit

---
+++ Day21.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Day21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        // Create linked list
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Traverse and print linked list
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        sc.close();
+    }
+}

68099cc00ab8f4a782c63294537f0a669fcaea29
Janvi25086 <janviii1505@gmail.com>
2026-07-22 20:45:05 +0530

    twenty six commit

---
+++ Day26.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class Day26 {
+
+    // Insert node at the end
+    static Node insert(Node head, int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+
+        return head;
+    }
+
+    // Traverse and print
+    static void traverse(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Node head = null;
+
+        for (int i = 0; i < n; i++) {
+            head = insert(head, sc.nextInt());
+        }
+
+        traverse(head);
+
+        sc.close();
+    }
+}

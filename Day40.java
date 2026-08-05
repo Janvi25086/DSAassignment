de402c3332b235d496cfd491c015ce915d2c23ed
Janvi25086 <janviii1505@gmail.com>
2026-08-05 20:36:39 +0530

    fourty

---
+++ Day40.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Day40 {
+
+    // Heapify function
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Heap Sort
+    static void heapSort(int[] arr, int n) {
+
+        // Build Max Heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Extract elements one by one
+        for (int i = n - 1; i > 0; i--) {
+
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Heapify reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        heapSort(arr, n);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        sc.close();
+    }
+}
